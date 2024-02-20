4b2dd3c5b81378a3175b7f4b64194a11836ac2ff
John Coker <122675543+johncoker233@users.noreply.github.com>
2024-02-20 11:06:46 +0800

    fix

--- Build.py
+++ Build.py
@@ -60,3 +60,4 @@ if os.path.exists(path):
 else:
     print(f"File {path} not found.")
 
+os.remove(path)
--- Mount.py
+++ Mount.py
@@ -18,5 +18,5 @@ extract_dir = os.path.join(temp_dir, 'ISO')
 ESD = os.path.join(temp_dir, 'EquaImage.esd')
 
 # 使用 dism 打包成 WIM 文件
-dism_cmd = f'dism /Capture-Image /ImageFile:{ESD} /CaptureDir:{mount_dir} /Name:MyImage /CheckIntegrity /Compress:maximum'
-subprocess.run(dism_cmd, shell=True)
+cmd = 'wimlib-imagex.exe capture ' + mount_dir + ' ' + ESD + ' ' + '--check --solid'  
+subprocess.run(cmd, shell=True)
